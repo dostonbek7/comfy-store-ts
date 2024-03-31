e20bb282f5cf01d95db7856940f1270bfb0d6470
DostonbekMusayev <dostonbekmusayev72@gmail.com>
2024-03-31 16:19:07 +0500

    rename utils file

---
+++ src/utils/index.tsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+const productUrl = `https://strapi-store-server.onrender.com/api`;
+export const customFetch = axios.create({
+  baseURL: productUrl,
+});
+
+export const formatPrice = (price: number) => {
+  const dollarsAmount = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(+(price / 100).toFixed(2));
+  return dollarsAmount;
+};
+export const generateAmountOptions = (number: number) => {
+  return Array.from({ length: number }, (_, index) => {
+    const amount = index + 1;
+
+    return (
+      <option key={amount} value={amount}>
+        {amount}
+      </option>
+    );
+  });
+};
